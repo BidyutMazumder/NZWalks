eca25e4a83ff102ae950a44395505e7326fe913e
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-09-18 18:10:51 +0600

    image004

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NZWalks.API.Models.DTO.Request;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+
+        public async Task<IActionResult> Upload([FromBody] ImageUploadRequestDto imageUploadRequestDto)
+        {
+            ValidateFileUpload(imageUploadRequestDto);
+            if (ModelState.IsValid)
+            {
+
+            }
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto imageUploadRequestDto)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (!allowedExtensions.Contains(Path.GetExtension(imageUploadRequestDto.File.FileName)))
+            {
+                ModelState.AddModelError("File","UnSupported File");
+            }
+            if(imageUploadRequestDto.File.Length > 5242880)
+            {
+                ModelState.AddModelError("File", "File size more then 5 mb");
+            }
+        }
+    }
+}
